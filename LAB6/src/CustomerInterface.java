8c55e9e8137531acf7da1d103598761cf3f41571
ansar798 <62190033+ansar798@users.noreply.github.com>
2021-06-04 22:05:25 -0400

    Git Error

--- LAB6/src/CustomerInterface.java
+++ LAB6/src/CustomerInterface.java
@@ -13,15 +13,12 @@ public class CustomerInterface {
     public static void main(String[] args) throws IOException{
 	final int NUM_MUTUAL_FUNDS = 7;
 	final int NUM_CUSTOMERS = 100;
-        HashTable<MutualFund> funds = new HashTable<>(NUM_MUTUAL_FUNDS * 2);
-	HashTable<Customer> customers = new HashTable<>(NUM_CUSTOMERS);
-		
+	HashTable<MutualFund> funds = new HashTable<>(NUM_MUTUAL_FUNDS * 2);
+	HashTable<Customer> customers = new HashTable<>(NUM_CUSTOMERS);	
 	DecimalFormat df = new DecimalFormat("###,##0.00");
-
-        String first, last, email, password, mutualName, ticker;
+	String first, last, email, password, mutualName, ticker;
 	double cash, sharePrice, numShares, fee;
-
-        File file1 = new File("mutual_funds.txt");
+	File file1 = new File("mutual_funds.txt");
 	File file2 = new File("customers.txt");
    }
 }
