394d4d718a4f9cec7a7f9d5f0c81ce99f1051962
ansar798 <62190033+ansar798@users.noreply.github.com>
2021-06-04 20:03:52 -0400

    MutualFund.java except equals() method everything done

--- LAB6/src/MutualFund.java
+++ LAB6/src/MutualFund.java
@@ -1,3 +1,4 @@
+
 /**
  * MutualFund.java
  * @author PARTNER_NAME_1
@@ -12,125 +13,143 @@ public class MutualFund {
 	private final String ticker;
 	private double pricePerShare;
 	private double tradingFee;
-	
-	/**CONSTRUCTORS*/
-	
+
+	/** CONSTRUCTORS */
+
 	/**
-	 * One arguement constructor,
-	 * when only ticker is known
-	 * @param ticker the ticker symbol
-	 * sets fundName to "No name"
-	 * and assigns 0 to pricePerShare
-	 * and tradingFee
+	 * One arguement constructor, when only ticker is known
+	 * 
+	 * @param ticker the ticker symbol sets fundName to "No name" and assigns 0 to
+	 *               pricePerShare and tradingFee
 	 */
 	public MutualFund(String ticker) {
-	
+		this.ticker = ticker;
+		this.fundName = "No Name";
+		this.pricePerShare = 0;
+		this.tradingFee = 0;
 	}
-	
+
 	/**
-        * One arguement constructor,
-        * when only name and ticker are known
-        * @param name the name of the fund
-        * @param ticker the ticker symbol
-        * Assigns 0 to pricePerShare
-        * and tradingFee
-        */
-       public MutualFund(String name, String ticker) {
-        
-       }
-	
+	 * two arguement constructor, when only name and ticker are known
+	 * 
+	 * @param name   the name of the fund
+	 * @param ticker the ticker symbol Assigns 0 to pricePerShare and tradingFee
+	 */
+	public MutualFund(String name, String ticker) {
+		this.ticker = ticker;
+		this.fundName = name;
+		this.pricePerShare = 0;
+		this.tradingFee = 0;
+	}
+
 	/**
-	 * Multi-argument constructor when all
-	 * MutualFund information is known
-	 * @param fundName the name of the fund
-	 * @param ticker the ticker symbol
+	 * Multi-argument constructor when all MutualFund information is known
+	 * 
+	 * @param fundName      the name of the fund
+	 * @param ticker        the ticker symbol
 	 * @param pricePerShare the price per share
-	 * @param tradingFee the trading fee as a percent
+	 * @param tradingFee    the trading fee as a percent
 	 */
 	public MutualFund(String fundName, String ticker, double pricePerShare, double tradingFee) {
-		
+		this.ticker = ticker;
+		this.fundName = fundName;
+		this.pricePerShare = pricePerShare;
+		this.tradingFee = tradingFee;
 	}
-	
-	/**ACCESSORS*/
+
+	/** ACCESSORS */
 
 	/**
 	 * Accesses the name of the fund
+	 * 
 	 * @return the fund name
 	 */
 	public String getFundName() {
-		return null;
+		return this.fundName;
 	}
 
 	/**
 	 * Accesses the ticker symbol
+	 * 
 	 * @return the ticker symbol
 	 */
 	public String getTicker() {
-		return null;
+		return this.ticker;
 	}
 
 	/**
 	 * Accesses the price per share
+	 * 
 	 * @return the price per share
 	 */
 	public double getPricePerShare() {
-		return 0.0;
+		return this.pricePerShare;
 	}
 
 	/**
 	 * Accesses the trading fee
+	 * 
 	 * @return the trading fee
 	 */
 	public double getTradingFee() {
-		return 0.0;
+		return this.tradingFee;
 	}
-	
-	/**MUTATORS*/
-	
+
+	/** MUTATORS */
+
 	/**
 	 * Updates the share price
+	 * 
 	 * @param pricePerShare the new share price
 	 */
 	public void setPricePerShare(double pricePerShare) {
-		
+		this.pricePerShare = pricePerShare;
 	}
-	
+
 	/**
 	 * Updates the trading fee
+	 * 
 	 * @param tradingFee the new trading fee
 	 */
 	public void setTradingFee(double tradingFee) {
+		this.tradingFee = tradingFee;
+	}
+
+	/** ADDITIONAL OPERATIONS */
+
+	@Override
+	public String toString() {
+		return fundName + "\n"
+				+ ticker + "\n"
+				+ "Share Price: $" + pricePerShare
+				+ "Trading Fee: " + tradingFee + "%";
+	}
+
+	/**
+	 * Compares this MutualFund to another Object for equality You must use the
+	 * formula presented in class for full credit (see Lesson 4)
+	 * 
+	 * @param o another Object (MutualFund or otherwise)
+	 * @return whether o is a MutualFund and has the same ticker as this MutualFund
+	 */
+	@Override
+	public boolean equals(Object o) {
 		
 	}
-	
-	/**ADDITIONAL OPERATIONS*/
 
-	@Override public String toString() {
-		return null;
-	}
-	
 	/**
-	 * Compares this MutualFund to
-	 * another Object for equality
-         * You must use the formula presented
-         * in class for full credit (see Lesson 4)
-	 * @param o another Object 
-	 * (MutualFund or otherwise)
-	 * @return whether o is a MutualFund
-	 * and has the same ticker as this MutualFund
+	 * Returns a consistent hash code for each MutualFund by summing the Unicode
+	 * values of each character in the key Key = ticker
+	 * 
+	 * @return the hash code
 	 */
-	@Override public boolean equals(Object o) {
-	    return false;
+	@Override
+	public int hashCode() {
+		String key = ticker;
+        int sum = 0;
+        for (int i = 0; i < key.length(); i++) {
+            sum += (int) key.charAt(i);
+        }
+        return sum;
 	}
-	
-	/**
-        * Returns a consistent hash code for
-        * each MutualFund by summing the Unicode values
-        * of each character in the key
-        * Key = ticker
-        * @return the hash code
-        */
-       @Override public int hashCode() {
-           return -1;
-       }
 }
